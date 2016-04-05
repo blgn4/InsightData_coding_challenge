06992d01b9c29133233f0ec21109db2bc771dc55
Niharika Bollapragada <blgniharika@hotmail.com>
2016-04-04 20:59:04 -0700

    Update run_JUnit.sh

--- insight_testsuite/run_JUnit.sh
+++ insight_testsuite/run_JUnit.sh
@@ -2,5 +2,5 @@
 
 #java -cp external_jars/json-simple-1.1.1.jar:external_jars/junit-4.12.jar:/home/worker2/my_coding_challenge/InsightData_coding_challenge/src/ org.junit.runner.JUnitCore Coding_Challenge.AverageDegreeTests
 
-javac -g  -cp ../external_jars/json-simple-1.1.1.jar:../external_jars/junit-4.12.jar ../src/Coding_Challenge/*.java
-java -cp ../external_jars/json-simple-1.1.1.jar:../external_jars/hamcrest-core-1.3.jar:../src/:../external_jars/junit-4.12.jar org.junit.runner.JUnitCore  Coding_Challenge.AverageDegreeTests
+javac -g  -cp ../src/json-simple-1.1.1.jar:../src/junit-4.12.jar ../src/Coding_Challenge/*.java
+java -cp ../src/json-simple-1.1.1.jar:../src/hamcrest-core-1.3.jar:../src/:../src/junit-4.12.jar org.junit.runner.JUnitCore  Coding_Challenge.AverageDegreeTests
